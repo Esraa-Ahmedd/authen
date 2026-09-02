8780a19d50950b332c7627bcd5013377ddb64cf2
Esraa Ahmed <esraa.ahmed.devx@gmail.com>
2026-09-02 04:00:31 +0300

    fix vercel deployment

--- app.js
+++ app.js
@@ -3,26 +3,24 @@ const app=express();
 import authrouter from "./Router/auth_router.js"
 import cookieParser from "cookie-parser";
  import bookRouter from "./Router/books_routes.js";
-
 app.use(express.json());
 app.use(cookieParser());
 app.use(express.static("public"));
 app.set('view engine','ejs')
  import dotenv from "dotenv"
 import mongoose from "mongoose";
-
  dotenv.config()
 
 const dsUrl=process.env.mongo_url;
 const PORT=process.env.PORT||3009;
-const secret=process.env.JWT_SECRET
+
  app.use("/", authrouter)
  app.use("/books", bookRouter);
 mongoose.connect(dsUrl)
 .then(()=>{
-    
+    app.listen(PORT,()=>{
   console.log("server running");
-  
+    })
   
     
 })
